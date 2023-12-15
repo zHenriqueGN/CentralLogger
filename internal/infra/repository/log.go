3bfc54ce60d57a2d4d6caea80fe0a61a3388962c
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-12-15 18:00:24 -0300

    feat: create log repository

---
+++ internal/infra/repository/log.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/zHenriqueGN/CentralLogger/internal/entity"
+	"github.com/zHenriqueGN/UnitOfWork/uow"
+)
+
+type LogRepository struct {
+	dbtx uow.DBTX
+}
+
+func (l *LogRepository) Save(ctx context.Context, system *entity.Log) error {
+	stmt, err := l.dbtx.PrepareContext(ctx, "INSERT INTO logs (id, system_id, level, status, message, time_stamp, user_id) ($1, $2, $3, $4, $5, $6, $7)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.ExecContext(ctx, system.ID.String(), system.SystemID, system.Level, system.Status, system.Message, system.TimeStamp, system.UserID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
