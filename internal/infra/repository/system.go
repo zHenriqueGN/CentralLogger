7f9937272b7a25615069a613e7af0968fce9c4d0
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-12-15 18:00:36 -0300

    feat: create system repository

---
+++ internal/infra/repository/system.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/zHenriqueGN/CentralLogger/internal/entity"
+	"github.com/zHenriqueGN/UnitOfWork/uow"
+)
+
+type SystemRepository struct {
+	dbtx uow.DBTX
+}
+
+func (s *SystemRepository) Create(ctx context.Context, system *entity.System) error {
+	stmt, err := s.dbtx.PrepareContext(ctx, "INSERT INTO systems (id, name, description, version) VALUES ($1, $2, $3, $4)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.ExecContext(ctx, system.ID.String(), system.Name, system.Description, system.Version)
+	if err != nil {
+		return err
+	}
+	return nil
+}
