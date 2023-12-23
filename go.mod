03e25f474dcb6d17b78eb970f4dc76164143adc7
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-12-23 11:43:35 -0300

    feat: implements events handlers

--- go.mod
+++ go.mod
@@ -5,15 +5,14 @@ go 1.21.5
 require (
 	github.com/google/uuid v1.4.0
 	github.com/lib/pq v1.10.9
+	github.com/rabbitmq/amqp091-go v1.9.0
 	github.com/stretchr/testify v1.8.4
 	github.com/zHenriqueGN/UnitOfWork v0.0.0-20231205170726-4a46d541963f
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/event/handler/log_saved_handler.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"sync"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/zHenriqueGN/CentralLogger/pkg/events"
+)
+
+type LogSavedHandler struct {
+	RabbitMQChannel *amqp.Channel
+}
+
+func NewLogSavedHandler(rabbitMQChannel *amqp.Channel) *LogSavedHandler {
+	return &LogSavedHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	}
+}
+
+func (h *LogSavedHandler) Handle(event events.EventInterface, wg *sync.WaitGroup) {
+	defer wg.Done()
+	jsonOutput, _ := json.Marshal(event.GetPayload())
+
+	message := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        jsonOutput,
+	}
+
+	h.RabbitMQChannel.PublishWithContext(
+		context.Background(),
+		"amq.direct",
+		"",
+		false,
+		false,
+		message,
+	)
+}
